99d91586294aa6f33fa21da0b25712b81287a840
semibroiled <amitavcmostafa@gmx.de>
2021-08-11 12:17:09 +0200

    Add user input

--- dice_roller.py
+++ dice_roller.py
@@ -2,10 +2,16 @@
 def main():
   import random
   dicesum=0
-  dice_rolls=2
+  dicesize=int(input('How many sides does you die have?\n'))
+  dice_rolls= int(input('How many dice will you roll?\n'))
   for i in range(0, dice_rolls):
-    roll = random.randint(1,6)
-    print(f'You rolled a {roll}')
+    roll = random.randint(1,dicesize)
+    if roll ==1:
+      print(f'Oopsies. You rolled a {roll}..Snake Eyes.rzzt')
+    elif roll == dicesize:
+      print(f'You rolled a {roll}! Critical success')
+    else:
+      print(f'You rolled a {roll}')
     dicesum+=roll
   print(f'You have rolled {dicesum}')
 
