a6b9d0997cdb32ff133995e16e647bd789163a06
semibroiled <amitavcmostafa@gmx.de>
2021-08-11 12:08:55 +0200

    dice roller function updated

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,13 @@
+
 def main():
-  print('You rolled a die')
+  import random
+  dicesum=0
+  dice_rolls=2
+  for i in range(0, dice_rolls):
+    roll = random.randint(1,6)
+    print(f'You rolled a {roll}')
+    dicesum+=roll
+  print(f'You have rolled {dicesum}')
 
 if __name__== "__main__":
   main()
